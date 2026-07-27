aa3cfced6b434fc33aaac607cd4d260ddb0681ff
Arnd Bergmann <arnd@arndb.de>
2026-07-27 12:47:35 +0200

    hwmon: mcp3021: remove old platform_data probe support
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/hwmon/mcp3021.c
+++ drivers/hwmon/mcp3021.c
@@ -133,20 +133,11 @@ static int mcp3021_probe(struct i2c_client *client)
 
 	i2c_set_clientdata(client, data);
 
-	if (np) {
-		if (!of_property_read_u32(np, "reference-voltage-microvolt",
-					  &data->vdd))
-			data->vdd /= 1000;
-		else
-			data->vdd = MCP3021_VDD_REF_DEFAULT;
-	} else {
-		u32 *pdata = dev_get_platdata(&client->dev);
-
-		if (pdata)
-			data->vdd = *pdata;
-		else
-			data->vdd = MCP3021_VDD_REF_DEFAULT;
-	}
+	if (!of_property_read_u32(np, "reference-voltage-microvolt",
+				  &data->vdd))
+		data->vdd /= 1000;
+	else
+		data->vdd = MCP3021_VDD_REF_DEFAULT;
 
 	type = (uintptr_t)i2c_get_match_data(client);
 	switch (type) {
@@ -183,19 +174,17 @@ static const struct i2c_device_id mcp3021_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, mcp3021_id);
 
-#ifdef CONFIG_OF
 static const struct of_device_id of_mcp3021_match[] = {
 	{ .compatible = "microchip,mcp3021", .data = (void *)mcp3021 },
 	{ .compatible = "microchip,mcp3221", .data = (void *)mcp3221 },
 	{ }
 };
 MODULE_DEVICE_TABLE(of, of_mcp3021_match);
-#endif
 
 static struct i2c_driver mcp3021_driver = {
 	.driver = {
 		.name = "mcp3021",
-		.of_match_table = of_match_ptr(of_mcp3021_match),
+		.of_match_table = of_mcp3021_match,
 	},
 	.probe = mcp3021_probe,
 	.id_table = mcp3021_id,
