b7d870fcec462e6fec18e74ea583115aee91298e
Arnd Bergmann <arnd@arndb.de>
2026-08-07 13:16:58 +0200

    pwm: xilinx: remove global header
    The xilinx pwm driver was merged in 2022 with a global header that
    exposes some private interfaces, with the intention of using those
    in a future clocksource patch. This never happened, so fold the
    contents into the driver itself to avoid the global header.
    
    Cc: Sean Anderson <sean.anderson@seco.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/pwm/pwm-xilinx.c
+++ drivers/pwm/pwm-xilinx.c
@@ -17,7 +17,6 @@
  * - Always produces low output if disabled.
  */
 
-#include <clocksource/timer-xilinx.h>
 #include <linux/clk.h>
 #include <linux/clk-provider.h>
 #include <linux/device.h>
@@ -27,12 +26,48 @@
 #include <linux/pwm.h>
 #include <linux/regmap.h>
 
+#define TCSR0	0x00
+#define TLR0	0x04
+#define TCR0	0x08
+#define TCSR1	0x10
+#define TLR1	0x14
+#define TCR1	0x18
+
+#define TCSR_MDT	BIT(0)
+#define TCSR_UDT	BIT(1)
+#define TCSR_GENT	BIT(2)
+#define TCSR_CAPT	BIT(3)
+#define TCSR_ARHT	BIT(4)
+#define TCSR_LOAD	BIT(5)
+#define TCSR_ENIT	BIT(6)
+#define TCSR_ENT	BIT(7)
+#define TCSR_TINT	BIT(8)
+#define TCSR_PWMA	BIT(9)
+#define TCSR_ENALL	BIT(10)
+#define TCSR_CASC	BIT(11)
+
+struct clk;
+struct device_node;
+struct regmap;
+
+/**
+ * struct xilinx_timer_priv - Private data for Xilinx AXI timer drivers
+ * @map: Regmap of the device, possibly with an offset
+ * @clk: Parent clock
+ * @max: Maximum value of the counters
+ */
+struct xilinx_timer_priv {
+	struct regmap *map;
+	struct clk *clk;
+	u64 max;
+};
+
 /*
  * The following functions are "common" to drivers for this device, and may be
  * exported at a future date.
  */
-u32 xilinx_timer_tlr_cycles(struct xilinx_timer_priv *priv, u32 tcsr,
-			    u64 cycles)
+static u32 xilinx_timer_tlr_cycles(struct xilinx_timer_priv *priv, u32 tcsr,
+				   u64 cycles)
 {
 	WARN_ON(cycles < 2 || cycles - 2 > priv->max);
 
@@ -41,8 +76,8 @@ u32 xilinx_timer_tlr_cycles(struct xilinx_timer_priv *priv, u32 tcsr,
 	return priv->max - cycles + 2;
 }
 
-unsigned int xilinx_timer_get_period(struct xilinx_timer_priv *priv,
-				     u32 tlr, u32 tcsr)
+static unsigned int xilinx_timer_get_period(struct xilinx_timer_priv *priv,
+					    u32 tlr, u32 tcsr)
 {
 	u64 cycles;
 
@@ -290,7 +325,7 @@ static struct platform_driver xilinx_pwm_driver = {
 	.probe = xilinx_pwm_probe,
 	.driver = {
 		.name = "xilinx-pwm",
-		.of_match_table = of_match_ptr(xilinx_pwm_of_match),
+		.of_match_table = xilinx_pwm_of_match,
 	},
 };
 module_platform_driver(xilinx_pwm_driver);
