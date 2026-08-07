3e74b78284ff01ac8af85f53b733d2f2f4d86985
Arnd Bergmann <arnd@arndb.de>
2026-08-07 12:54:11 +0200

    ASoC: ab8500: remove pdata based probing
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- sound/soc/codecs/ab8500-codec.c
+++ sound/soc/codecs/ab8500-codec.c
@@ -27,7 +27,6 @@
 #include <linux/mfd/abx500/ab8500.h>
 #include <linux/mfd/abx500.h>
 #include <linux/mfd/abx500/ab8500-sysctrl.h>
-#include <linux/mfd/abx500/ab8500-codec.h>
 #include <linux/regulator/consumer.h>
 #include <linux/of.h>
 
@@ -40,6 +39,44 @@
 #include <sound/tlv.h>
 
 #include "ab8500-codec.h"
+
+/* Mic-types */
+enum amic_type {
+	AMIC_TYPE_SINGLE_ENDED,
+	AMIC_TYPE_DIFFERENTIAL
+};
+
+/* Mic-biases */
+enum amic_micbias {
+	AMIC_MICBIAS_VAMIC1,
+	AMIC_MICBIAS_VAMIC2,
+	AMIC_MICBIAS_UNKNOWN
+};
+
+/* Bias-voltage */
+enum ear_cm_voltage {
+	EAR_CMV_0_95V,
+	EAR_CMV_1_10V,
+	EAR_CMV_1_27V,
+	EAR_CMV_1_58V,
+	EAR_CMV_UNKNOWN
+};
+
+/* Analog microphone settings */
+struct amic_settings {
+	enum amic_type mic1_type;
+	enum amic_type mic2_type;
+	enum amic_micbias mic1a_micbias;
+	enum amic_micbias mic1b_micbias;
+	enum amic_micbias mic2_micbias;
+};
+
+/* Platform data structure for the audio-parts of the AB8500 */
+struct ab8500_codec_platform_data {
+	struct amic_settings amics;
+	enum ear_cm_voltage ear_cmv;
+};
+
 
 /* Macrocell value definitions */
 #define CLK_32K_OUT2_DISABLE			0x01
