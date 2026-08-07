d29723d9e9e29e17d3b48c1b2824c75d806d6bbe
Arnd Bergmann <arnd@arndb.de>
2026-08-07 14:32:52 +0200

    hwmon: gsc: remove pdata based probing
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/hwmon/gsc-hwmon.c
+++ drivers/hwmon/gsc-hwmon.c
@@ -13,14 +13,53 @@
 #include <linux/regmap.h>
 #include <linux/slab.h>
 
-#include <linux/platform_data/gsc_hwmon.h>
-
 #define GSC_HWMON_MAX_TEMP_CH	16
 #define GSC_HWMON_MAX_IN_CH	16
 #define GSC_HWMON_MAX_FAN_CH	16
 
 #define GSC_HWMON_RESOLUTION	12
 #define GSC_HWMON_VREF		2500
+
+enum gsc_hwmon_mode {
+	mode_temperature,
+	mode_voltage_24bit,
+	mode_voltage_raw,
+	mode_voltage_16bit,
+	mode_fan,
+	mode_max,
+};
+
+/**
+ * struct gsc_hwmon_channel - configuration parameters
+ * @reg:  I2C register offset
+ * @mode: channel mode
+ * @name: channel name
+ * @mvoffset: voltage offset
+ * @vdiv: voltage divider array (2 resistor values in milli-ohms)
+ */
+struct gsc_hwmon_channel {
+	unsigned int reg;
+	unsigned int mode;
+	const char *name;
+	unsigned int mvoffset;
+	unsigned int vdiv[2];
+};
+
+/**
+ * struct gsc_hwmon_platform_data - platform data for gsc_hwmon driver
+ * @nchannels:	number of elements in @channels array
+ * @vreference: voltage reference (mV)
+ * @resolution: ADC bit resolution
+ * @fan_base: register base for FAN controller
+ * @channels:	array of gsc_hwmon_channel structures describing channels
+ */
+struct gsc_hwmon_platform_data {
+	int nchannels;
+	unsigned int resolution;
+	unsigned int vreference;
+	unsigned int fan_base;
+	struct gsc_hwmon_channel channels[] __counted_by(nchannels);
+};
 
 struct gsc_hwmon_data {
 	struct gsc_dev *gsc;
@@ -303,16 +342,14 @@ static int gsc_hwmon_probe(struct platform_device *pdev)
 	struct gsc_dev *gsc = dev_get_drvdata(pdev->dev.parent);
 	struct device *dev = &pdev->dev;
 	struct device *hwmon_dev;
-	struct gsc_hwmon_platform_data *pdata = dev_get_platdata(dev);
+	struct gsc_hwmon_platform_data *pdata;
 	struct gsc_hwmon_data *hwmon;
 	const struct attribute_group **groups;
 	int i, i_in, i_temp, i_fan;
 
-	if (!pdata) {
-		pdata = gsc_hwmon_get_devtree_pdata(dev);
-		if (IS_ERR(pdata))
-			return PTR_ERR(pdata);
-	}
+	pdata = gsc_hwmon_get_devtree_pdata(dev);
+	if (IS_ERR(pdata))
+		return PTR_ERR(pdata);
 
 	hwmon = devm_kzalloc(dev, sizeof(*hwmon), GFP_KERNEL);
 	if (!hwmon)
