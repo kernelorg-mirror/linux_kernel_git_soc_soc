9476cd5b314331fca0364cfa40c2f1048f179aed
Arnd Bergmann <arnd@arndb.de>
2026-08-07 13:25:01 +0200

    clk: si5351: remove pdata based probing
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/clk/clk-si5351.c
+++ drivers/clk/clk-si5351.c
@@ -22,13 +22,121 @@
 #include <linux/rational.h>
 #include <linux/i2c.h>
 #include <linux/of.h>
-#include <linux/platform_data/si5351.h>
 #include <linux/regmap.h>
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <asm/div64.h>
 
 #include "clk-si5351.h"
+
+/**
+ * enum si5351_pll_src - Si5351 pll clock source
+ * @SI5351_PLL_SRC_DEFAULT: default, do not change eeprom config
+ * @SI5351_PLL_SRC_XTAL: pll source clock is XTAL input
+ * @SI5351_PLL_SRC_CLKIN: pll source clock is CLKIN input (Si5351C only)
+ */
+enum si5351_pll_src {
+	SI5351_PLL_SRC_DEFAULT = 0,
+	SI5351_PLL_SRC_XTAL = 1,
+	SI5351_PLL_SRC_CLKIN = 2,
+};
+
+/**
+ * enum si5351_multisynth_src - Si5351 multisynth clock source
+ * @SI5351_MULTISYNTH_SRC_DEFAULT: default, do not change eeprom config
+ * @SI5351_MULTISYNTH_SRC_VCO0: multisynth source clock is VCO0
+ * @SI5351_MULTISYNTH_SRC_VCO1: multisynth source clock is VCO1/VXCO
+ */
+enum si5351_multisynth_src {
+	SI5351_MULTISYNTH_SRC_DEFAULT = 0,
+	SI5351_MULTISYNTH_SRC_VCO0 = 1,
+	SI5351_MULTISYNTH_SRC_VCO1 = 2,
+};
+
+/**
+ * enum si5351_clkout_src - Si5351 clock output clock source
+ * @SI5351_CLKOUT_SRC_DEFAULT: default, do not change eeprom config
+ * @SI5351_CLKOUT_SRC_MSYNTH_N: clkout N source clock is multisynth N
+ * @SI5351_CLKOUT_SRC_MSYNTH_0_4: clkout N source clock is multisynth 0 (N<4)
+ *                                or 4 (N>=4)
+ * @SI5351_CLKOUT_SRC_XTAL: clkout N source clock is XTAL
+ * @SI5351_CLKOUT_SRC_CLKIN: clkout N source clock is CLKIN (Si5351C only)
+ */
+enum si5351_clkout_src {
+	SI5351_CLKOUT_SRC_DEFAULT = 0,
+	SI5351_CLKOUT_SRC_MSYNTH_N = 1,
+	SI5351_CLKOUT_SRC_MSYNTH_0_4 = 2,
+	SI5351_CLKOUT_SRC_XTAL = 3,
+	SI5351_CLKOUT_SRC_CLKIN = 4,
+};
+
+/**
+ * enum si5351_drive_strength - Si5351 clock output drive strength
+ * @SI5351_DRIVE_DEFAULT: default, do not change eeprom config
+ * @SI5351_DRIVE_2MA: 2mA clock output drive strength
+ * @SI5351_DRIVE_4MA: 4mA clock output drive strength
+ * @SI5351_DRIVE_6MA: 6mA clock output drive strength
+ * @SI5351_DRIVE_8MA: 8mA clock output drive strength
+ */
+enum si5351_drive_strength {
+	SI5351_DRIVE_DEFAULT = 0,
+	SI5351_DRIVE_2MA = 2,
+	SI5351_DRIVE_4MA = 4,
+	SI5351_DRIVE_6MA = 6,
+	SI5351_DRIVE_8MA = 8,
+};
+
+/**
+ * enum si5351_disable_state - Si5351 clock output disable state
+ * @SI5351_DISABLE_DEFAULT: default, do not change eeprom config
+ * @SI5351_DISABLE_LOW: CLKx is set to a LOW state when disabled
+ * @SI5351_DISABLE_HIGH: CLKx is set to a HIGH state when disabled
+ * @SI5351_DISABLE_FLOATING: CLKx is set to a FLOATING state when
+ *				disabled
+ * @SI5351_DISABLE_NEVER: CLKx is NEVER disabled
+ */
+enum si5351_disable_state {
+	SI5351_DISABLE_DEFAULT = 0,
+	SI5351_DISABLE_LOW,
+	SI5351_DISABLE_HIGH,
+	SI5351_DISABLE_FLOATING,
+	SI5351_DISABLE_NEVER,
+};
+
+/**
+ * struct si5351_clkout_config - Si5351 clock output configuration
+ * @clkout: clkout number
+ * @multisynth_src: multisynth source clock
+ * @clkout_src: clkout source clock
+ * @pll_master: if true, clkout can also change pll rate
+ * @pll_reset: if true, clkout can reset its pll
+ * @drive: output drive strength
+ * @rate: initial clkout rate, or default if 0
+ */
+struct si5351_clkout_config {
+	enum si5351_multisynth_src multisynth_src;
+	enum si5351_clkout_src clkout_src;
+	enum si5351_drive_strength drive;
+	enum si5351_disable_state disable_state;
+	bool pll_master;
+	bool pll_reset;
+	unsigned long rate;
+};
+
+/**
+ * struct si5351_platform_data - Platform data for the Si5351 clock driver
+ * @clk_xtal: xtal input clock
+ * @clk_clkin: clkin input clock
+ * @pll_src: array of pll source clock setting
+ * @pll_reset: array indicating if plls should be reset after setting the rate
+ * @clkout: array of clkout configuration
+ */
+struct si5351_platform_data {
+	enum si5351_pll_src pll_src[2];
+	bool pll_reset[2];
+	struct si5351_clkout_config clkout[8];
+};
+
 
 struct si5351_driver_data;
 
@@ -1159,7 +1267,6 @@ static const struct clk_ops si5351_clkout_ops = {
 /*
  * Si5351 i2c probe and DT
  */
-#ifdef CONFIG_OF
 static const struct of_device_id si5351_dt_ids[] = {
 	{ .compatible = "silabs,si5351a", .data = (void *)SI5351_VARIANT_A, },
 	{ .compatible = "silabs,si5351a-msop",
@@ -1411,18 +1518,6 @@ si53351_of_clk_get(struct of_phandle_args *clkspec, void *data)
 
 	return &drvdata->clkout[idx].hw;
 }
-#else
-static int si5351_dt_parse(struct i2c_client *client, enum si5351_variant variant)
-{
-	return 0;
-}
-
-static struct clk_hw *
-si53351_of_clk_get(struct of_phandle_args *clkspec, void *data)
-{
-	return NULL;
-}
-#endif /* CONFIG_OF */
 
 static const struct i2c_device_id si5351_i2c_ids[] = {
 	{ "si5351a", SI5351_VARIANT_A },
@@ -1711,7 +1806,7 @@ static int si5351_i2c_probe(struct i2c_client *client)
 static struct i2c_driver si5351_driver = {
 	.driver = {
 		.name = "si5351",
-		.of_match_table = of_match_ptr(si5351_dt_ids),
+		.of_match_table = si5351_dt_ids,
 	},
 	.probe = si5351_i2c_probe,
 	.id_table = si5351_i2c_ids,
