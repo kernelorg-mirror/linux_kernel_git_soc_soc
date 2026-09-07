0473e9cc0f2b3ed2a03d5ac1c7f562c9b3010eda
Arnd Bergmann <arnd@arndb.de>
2026-09-07 16:23:24 +0200

    media: i2c: lm3646: remove platform_data based probing
    There are no in-tree users of lm3646, but it is possible to use it
    through a custom dtb file. Since there is no platform_data available,
    make that the only option and clean up the legacy code.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/media/i2c/lm3646.c
+++ drivers/media/i2c/lm3646.c
@@ -15,9 +15,78 @@
 #include <linux/slab.h>
 #include <linux/regmap.h>
 #include <linux/videodev2.h>
-#include <media/i2c/lm3646.h>
 #include <media/v4l2-ctrls.h>
 #include <media/v4l2-device.h>
+#include <media/v4l2-subdev.h>
+
+#define LM3646_NAME	"lm3646"
+#define LM3646_I2C_ADDR_REV1	(0x67)
+#define LM3646_I2C_ADDR_REV0	(0x63)
+
+/*  TOTAL FLASH Brightness Max
+ *	min 93350uA, step 93750uA, max 1499600uA
+ */
+#define LM3646_TOTAL_FLASH_BRT_MIN 93350
+#define LM3646_TOTAL_FLASH_BRT_STEP 93750
+#define LM3646_TOTAL_FLASH_BRT_MAX 1499600
+#define LM3646_TOTAL_FLASH_BRT_uA_TO_REG(a)	\
+	((a) < LM3646_TOTAL_FLASH_BRT_MIN ? 0 :	\
+	 ((((a) - LM3646_TOTAL_FLASH_BRT_MIN) / LM3646_TOTAL_FLASH_BRT_STEP)))
+
+/*  TOTAL TORCH Brightness Max
+ *	min 23040uA, step 23430uA, max 187100uA
+ */
+#define LM3646_TOTAL_TORCH_BRT_MIN 23040
+#define LM3646_TOTAL_TORCH_BRT_STEP 23430
+#define LM3646_TOTAL_TORCH_BRT_MAX 187100
+#define LM3646_TOTAL_TORCH_BRT_uA_TO_REG(a)	\
+	((a) < LM3646_TOTAL_TORCH_BRT_MIN ? 0 :	\
+	 ((((a) - LM3646_TOTAL_TORCH_BRT_MIN) / LM3646_TOTAL_TORCH_BRT_STEP)))
+
+/*  LED1 FLASH Brightness
+ *	min 23040uA, step 11718uA, max 1499600uA
+ */
+#define LM3646_LED1_FLASH_BRT_MIN 23040
+#define LM3646_LED1_FLASH_BRT_STEP 11718
+#define LM3646_LED1_FLASH_BRT_MAX 1499600
+#define LM3646_LED1_FLASH_BRT_uA_TO_REG(a)	\
+	((a) <= LM3646_LED1_FLASH_BRT_MIN ? 0 :	\
+	 ((((a) - LM3646_LED1_FLASH_BRT_MIN) / LM3646_LED1_FLASH_BRT_STEP))+1)
+
+/*  LED1 TORCH Brightness
+ *	min 2530uA, step 1460uA, max 187100uA
+ */
+#define LM3646_LED1_TORCH_BRT_MIN 2530
+#define LM3646_LED1_TORCH_BRT_STEP 1460
+#define LM3646_LED1_TORCH_BRT_MAX 187100
+#define LM3646_LED1_TORCH_BRT_uA_TO_REG(a)	\
+	((a) <= LM3646_LED1_TORCH_BRT_MIN ? 0 :	\
+	 ((((a) - LM3646_LED1_TORCH_BRT_MIN) / LM3646_LED1_TORCH_BRT_STEP))+1)
+
+/*  FLASH TIMEOUT DURATION
+ *	min 50ms, step 50ms, max 400ms
+ */
+#define LM3646_FLASH_TOUT_MIN 50
+#define LM3646_FLASH_TOUT_STEP 50
+#define LM3646_FLASH_TOUT_MAX 400
+#define LM3646_FLASH_TOUT_ms_TO_REG(a)	\
+	((a) <= LM3646_FLASH_TOUT_MIN ? 0 :	\
+	 (((a) - LM3646_FLASH_TOUT_MIN) / LM3646_FLASH_TOUT_STEP))
+
+/* struct lm3646_platform_data
+ *
+ * @flash_timeout: flash timeout
+ * @led1_flash_brt: led1 flash mode brightness, uA
+ * @led1_torch_brt: led1 torch mode brightness, uA
+ */
+struct lm3646_platform_data {
+
+	u32 flash_timeout;
+
+	u32 led1_flash_brt;
+	u32 led1_torch_brt;
+};
+
 
 /* registers definitions */
 #define REG_ENABLE		0x01
@@ -337,7 +406,7 @@ static int lm3646_init_device(struct lm3646_flash *flash)
 static int lm3646_probe(struct i2c_client *client)
 {
 	struct lm3646_flash *flash;
-	struct lm3646_platform_data *pdata = dev_get_platdata(&client->dev);
+	struct lm3646_platform_data *pdata;
 	int rval;
 
 	flash = devm_kzalloc(&client->dev, sizeof(*flash), GFP_KERNEL);
@@ -349,17 +418,15 @@ static int lm3646_probe(struct i2c_client *client)
 		return PTR_ERR(flash->regmap);
 
 	/* check device tree if there is no platform data */
-	if (pdata == NULL) {
-		pdata = devm_kzalloc(&client->dev,
-				     sizeof(struct lm3646_platform_data),
-				     GFP_KERNEL);
-		if (pdata == NULL)
-			return -ENOMEM;
-		/* use default data in case of no platform data */
-		pdata->flash_timeout = LM3646_FLASH_TOUT_MAX;
-		pdata->led1_torch_brt = LM3646_LED1_TORCH_BRT_MAX;
-		pdata->led1_flash_brt = LM3646_LED1_FLASH_BRT_MAX;
-	}
+	pdata = devm_kzalloc(&client->dev,
+			     sizeof(struct lm3646_platform_data),
+			     GFP_KERNEL);
+	if (pdata == NULL)
+		return -ENOMEM;
+
+	pdata->flash_timeout = LM3646_FLASH_TOUT_MAX;
+	pdata->led1_torch_brt = LM3646_LED1_TORCH_BRT_MAX;
+	pdata->led1_flash_brt = LM3646_LED1_FLASH_BRT_MAX;
 	flash->pdata = pdata;
 	flash->dev = &client->dev;
 
