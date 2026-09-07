31dcfc8d78128b8372606797ad361bcd754ce736
Arnd Bergmann <arnd@arndb.de>
2026-09-07 16:22:27 +0200

    usb: ehci-orion: remove platform_data based probing
    The orion/mvebu platform now always uses devicetree based probing,
    so remove the legacy code path as a cleanup.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/usb/host/ehci-orion.c
+++ drivers/usb/host/ehci-orion.c
@@ -10,7 +10,6 @@
 #include <linux/platform_device.h>
 #include <linux/mbus.h>
 #include <linux/clk.h>
-#include <linux/platform_data/usb-ehci-orion.h>
 #include <linux/of.h>
 #include <linux/usb.h>
 #include <linux/usb/hcd.h>
@@ -71,76 +70,6 @@ static struct hc_driver __read_mostly ehci_orion_hc_driver;
  */
 static const u64 dma_mask_orion =	DMA_BIT_MASK(32);
 static const u64 dma_mask_ac5 =		DMA_BIT_MASK(34);
-
-/*
- * Implement Orion USB controller specification guidelines
- */
-static void orion_usb_phy_v1_setup(struct usb_hcd *hcd)
-{
-	/* The below GLs are according to the Orion Errata document */
-	/*
-	 * Clear interrupt cause and mask
-	 */
-	wrl(USB_CAUSE, 0);
-	wrl(USB_MASK, 0);
-
-	/*
-	 * Reset controller
-	 */
-	wrl(USB_CMD, rdl(USB_CMD) | USB_CMD_RESET);
-	while (rdl(USB_CMD) & USB_CMD_RESET);
-
-	/*
-	 * GL# USB-10: Set IPG for non start of frame packets
-	 * Bits[14:8]=0xc
-	 */
-	wrl(USB_IPG, (rdl(USB_IPG) & ~0x7f00) | 0xc00);
-
-	/*
-	 * GL# USB-9: USB 2.0 Power Control
-	 * BG_VSEL[7:6]=0x1
-	 */
-	wrl(USB_PHY_PWR_CTRL, (rdl(USB_PHY_PWR_CTRL) & ~0xc0)| 0x40);
-
-	/*
-	 * GL# USB-1: USB PHY Tx Control - force calibration to '8'
-	 * TXDATA_BLOCK_EN[21]=0x1, EXT_RCAL_EN[13]=0x1, IMP_CAL[6:3]=0x8
-	 */
-	wrl(USB_PHY_TX_CTRL, (rdl(USB_PHY_TX_CTRL) & ~0x78) | 0x202040);
-
-	/*
-	 * GL# USB-3 GL# USB-9: USB PHY Rx Control
-	 * RXDATA_BLOCK_LENGHT[31:30]=0x3, EDGE_DET_SEL[27:26]=0,
-	 * CDR_FASTLOCK_EN[21]=0, DISCON_THRESHOLD[9:8]=0, SQ_THRESH[7:4]=0x1
-	 */
-	wrl(USB_PHY_RX_CTRL, (rdl(USB_PHY_RX_CTRL) & ~0xc2003f0) | 0xc0000010);
-
-	/*
-	 * GL# USB-3 GL# USB-9: USB PHY IVREF Control
-	 * PLLVDD12[1:0]=0x2, RXVDD[5:4]=0x3, Reserved[19]=0
-	 */
-	wrl(USB_PHY_IVREF_CTRL, (rdl(USB_PHY_IVREF_CTRL) & ~0x80003 ) | 0x32);
-
-	/*
-	 * GL# USB-3 GL# USB-9: USB PHY Test Group Control
-	 * REG_FIFO_SQ_RST[15]=0
-	 */
-	wrl(USB_PHY_TST_GRP_CTRL, rdl(USB_PHY_TST_GRP_CTRL) & ~0x8000);
-
-	/*
-	 * Stop and reset controller
-	 */
-	wrl(USB_CMD, rdl(USB_CMD) & ~USB_CMD_RUN);
-	wrl(USB_CMD, rdl(USB_CMD) | USB_CMD_RESET);
-	while (rdl(USB_CMD) & USB_CMD_RESET);
-
-	/*
-	 * GL# USB-5 Streaming disable REG_USB_MODE[4]=1
-	 * TBD: This need to be done after each reset!
-	 * GL# USB-4 Setup USB Host mode
-	 */
-	wrl(USB_MODE, USB_MODE_SDIS | USB_MODE_HOST);
-}
 
 static void
 ehci_orion_conf_mbus_windows(struct usb_hcd *hcd,
@@ -209,14 +138,12 @@ static const struct ehci_driver_overrides orion_overrides __initconst = {
 
 static int ehci_orion_drv_probe(struct platform_device *pdev)
 {
-	struct orion_ehci_data *pd = dev_get_platdata(&pdev->dev);
 	const struct mbus_dram_target_info *dram;
 	struct resource *res;
 	struct usb_hcd *hcd;
 	struct ehci_hcd *ehci;
 	void __iomem *regs;
 	int irq, err;
-	enum orion_ehci_phy_ver phy_version;
 	struct orion_ehci_hcd *priv;
 	u64 *dma_mask_ptr;
 
@@ -280,26 +207,6 @@ static int ehci_orion_drv_probe(struct platform_device *pdev)
 	dram = mv_mbus_dram_info();
 	if (dram)
 		ehci_orion_conf_mbus_windows(hcd, dram);
-
-	/*
-	 * setup Orion USB controller.
-	 */
-	if (pdev->dev.of_node)
-		phy_version = EHCI_PHY_NA;
-	else
-		phy_version = pd->phy_version;
-
-	switch (phy_version) {
-	case EHCI_PHY_NA:	/* dont change USB phy settings */
-		break;
-	case EHCI_PHY_ORION:
-		orion_usb_phy_v1_setup(hcd);
-		break;
-	case EHCI_PHY_DD:
-	case EHCI_PHY_KW:
-	default:
-		dev_warn(&pdev->dev, "USB phy version isn't supported.\n");
-	}
 
 	err = usb_add_hcd(hcd, irq, IRQF_SHARED);
 	if (err)
